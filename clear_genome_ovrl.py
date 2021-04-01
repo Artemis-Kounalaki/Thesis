f421c8834675383aeba9fe6bff783ea88c4f1908
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2021-04-01 17:13:28 +0300

    Updated.

---
+++ clear_genome_ovrl.py
@@ -0,0 +1,21 @@
+import ast
+import os
+
+def clear_genome(path,reference,overlapped,save):
+    os.chdir(os.path.expanduser(path))
+    with open(reference) as f:
+        list2 = f.readlines()
+
+    with open(overlapped) as file:
+        gene_names = ast.literal_eval(file.read())
+
+    for line in range(len(list2)):
+        if any(gene in list2[line] for gene in gene_names):
+            list2[line]=' '
+            if line == len(list2)-1:
+                break
+            list2[line+1]=' '
+
+    with open(save, 'a') as outfile:
+        for sublist in list2:
+           outfile.write('{}\n'.format(sublist))
---
+++ ov_ids_new_h.py
@@ -0,0 +1,3 @@
+from ov_ids_new import ov_ids
+
+ov_ids('~/conserved_gene_order1/human_reference','GRCh38','ids.txt','overlapped_ids.txt','clean_ids.txt')
