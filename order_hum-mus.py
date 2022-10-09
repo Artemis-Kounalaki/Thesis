f73216546e9ce3017cf3e1ab6ffdb4ce4bda49b6
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-09 20:26:55 +0300

    .

---
+++ order_hum-mus.py
@@ -0,0 +1,3 @@
+from order import order
+
+order('~/conserved_gene_order1/mus_reference','reciprocal_h-mus.txt','~/conserved_gene_order1/human_reference','clean_ids.txt','~/conserved_gene_order1/mus_reference','clean_ids_mus.txt','~/conserved_gene_order1/mus_reference/CGO.txt','~/conserved_gene_order1/mus_reference/nCGO.txt')
