75dc7db04de02aeaecd9608b045e99eb1a656d3f
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-13 15:08:51 +0300

    .

---
+++ GSE_before.py
@@ -0,0 +1,75 @@
+import pandas as pd
+from scipy import stats
+
+# Load RBH results with GCO info (1:GCO,0:NGCO)
+df_al = pd.read_csv('CGO_nCGO_h-mac.txt', sep='\t')
+df_al.columns=['qseqid', 'sseqid', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore', 'slen', 'qlen','Conserved']
+print(df_al)
+
+# Keep only human proteins have same hit on macaca
+df_dou = df_al[df_al.duplicated(subset=['sseqid'], keep=False)]
+df_dou= df_dou.reset_index(drop=True)
+print(df_dou.sort_values('sseqid'))
+
+
+# Cons/ non Cons
+
+one=df_dou[df_dou.Conserved == 1]
+two=df_dou[df_dou.Conserved == 0]
+print(sum(one.pident)/len(one))
+print(sum(two.pident)/len(two))
+print(stats.mannwhitneyu(one.pident, two.pident))
+
+# Their average pr identity & pvalue
+target_c = df_dou[df_dou.Conserved == 1]
+target_nc = df_dou[df_dou.Conserved == 0]
+
+# Total num of cons
+print(target_c)
+
+# Total num of NON cons
+print(target_nc)
+
+
+# Non uniform group number of groups
+keep=set(one['sseqid']).intersection(two['sseqid'])
+print(len(keep))
+
+# Total number of non uniform prot
+inte=df_dou[df_dou.sseqid.isin(keep)]
+
+# Num of con in non uni
+c_nuni=inte[inte.Conserved ==1].qseqid
+print(c_nuni)
+
+# Num of non cons in non uni
+nc_nuni=inte[inte.Conserved ==0].qseqid
+print(nc_nuni)
+
+
+# Total number  in uniform prot
+res=df_dou[~df_dou.sseqid.isin(keep)]
+print(res)
+
+
+# Groups of cons in uniform
+print(len(set(res[res.Conserved ==1].sseqid)))
+# Groups of non cons in uniform
+print(len(set(res[res.Conserved ==0].sseqid)))
+
+
+# Num of con in uni
+c_uni=res[res.Conserved ==1].qseqid
+print(c_uni)
+
+# Num of non cons in uni
+nc_uni=res[res.Conserved ==0].qseqid
+print(nc_uni)
+
+# Save cons/ncons non uni group
+c_nuni.to_csv('~/c_nuni.txt', header=None, index=None)
+nc_nuni.to_csv('~/nc_nuni.txt', header=None, index=None)
+
+#Save cons/ncons uni groups
+c_uni.to_csv('~/c_uni.txt', header=None, index=None)
+nc_uni.to_csv('~/nc_uni.txt', header=None, index=None)
--- geo_an.r
+++ geo_an.r
@@ -1,77 +1,57 @@
 BiocManager::install("GEOquery")
+BiocManager::install("hgu133a.db")
+
 library(GEOquery)
+library(hgu133a.db)
+library(gprofiler2)
 
 # Parse the GEO  study of interest
 gset <- getGEO("GSE2361", GSEMatrix =TRUE, getGPL=FALSE)[[1]]
+dim(gset)
+#Check if data are normalized
+boxplot(exprs(gset),outline=FALSE)
+hist(log2(exprs(gset)))
+exprs(gset) <- log2(exprs(gset))
 # Get the features & samples num.
 dim(gset)
 # Get access in all probe names.
 str(gset)
-# Check for normalization
+# Check
 summary(exprs(gset))
-# Log transform the table
-exprs(gset) <- log2(exprs(gset))
 exprs(gset)
 
-# See now the normalised counts
-boxplot(exprs(gset),outline=FALSE)
 # Save the tissue series
 tis=c(gset@phenoData@data[["description"]])
 
 
-
-
-# Install the human database with probe ids
-BiocManager::install("hgu133plus2.db")
-library(hgu133plus2.db)
 # Convert probe ids into gene symbols
-geneSymbols <- select(hgu133plus2.db, keys=rownames(exprs(gset)), column="SYMBOL", keytype="PROBEID", multiVals="filter")
+geneSymbols <- mapIds(hgu133a.db, keys=rownames(exprs(gset)), column="ENSEMBL", keytype="PROBEID", multiVals="filter")
+geneSymbols
 # Convert the result into df
-df <- data.frame(matrix(unlist(geneSymbols), ncol =length(geneSymbols)))
-# Remove dublicates
-df = df[!duplicated(df$X1),]
-
+df <- stack(geneSymbols)
 # Copy the gene symbols into the geo matrix
-new_m <- cbind(df$X2, exprs(gset))
+new_m <- cbind(df$values, exprs(gset))
 # Make gene symbols rownames instead of the probe ids
 colnames(new_m)[1] <- 'GENE'
 rownames(new_m) <- new_m[,1]
 new_m=new_m[,-1]
 colnames(new_m) <- tis
+
 # In order to look our list of genes in interest:
-
-library(gprofiler2)
-# Parse the txt file as a table with proteins of interest
-cons_par0 <- read.table(file = "cons_par0.txt", sep = "\n")
-# Make it a vector and remove the after . numbers
-cons_par0=unlist(cons_par0)
-names(cons_par0)=NULL
-cons_par0=gsub("\\..*","",cons_par0)
-#!!
-write.csv(cons_par0, file = "CONS_1.csv")
-# Convert the proteins to genes
-gp_cons_par0 = gconvert(cons_par0, organism = "hsapiens", target="HGNC")
-# Make genes in a vector
-gp_cons_par0=unlist(gp_cons_par0["target"])
-names(gp_cons_par0)=NULL
-gp_cons_par0[1]
-new_m=new_m[!duplicated(new_m[,1]),]
-new_m=new_m[new_m[,1] %in% gp_cons_par0,]
-length(new_m[1,])
-nrow(new_m)
-
-# THis step
-library(gprofiler2)
-cgo_ncgo <- read.table(file = "cgo_ncgo_par.txt", sep = " ")
+setwd("/Users/artemiskounalake")
+cgo_ncgo <- read.table(file = "c_nuni.txt", sep = "\t")
 cgo_ncgo <- lapply(cgo_ncgo, gsub, pattern = "\\..*", replacement = "")
 v1=unlist(cgo_ncgo[1])
-v1 = gconvert(v1, organism = "hsapiens", target="HGNC")
+v1 = gconvert(v1, organism = "hsapiens", target="ENSG")
 v1=unlist(v1["target"])
 names(v1)=NULL
-v2=unlist(cgo_ncgo[2])
-v2 = gconvert(v2, organism = "hsapiens", target="HGNC")
+v1
+cgo_ncgo1 <- read.table(file = "nc_nuni.txt", sep = "\t")
+cgo_ncgo1 <- lapply(cgo_ncgo1, gsub, pattern = "\\..*", replacement = "")
+v2=unlist(cgo_ncgo1[1])
+v2 = gconvert(v2, organism = "hsapiens", target="ENSG")
 v2=unlist(v2["target"])
 names(v2)=NULL
-write.table(v1, file = "target_cgo.txt", row.names =FALSE, col.names = FALSE )
-write.table(v2, file = "target_ncgo.txt", row.names =FALSE, col.names = FALSE )
+write.table(v1, file = "c_nuni_2971.txt", row.names =FALSE, col.names = FALSE )
+write.table(v2, file = "nc_nuni_2971.txt", row.names =FALSE, col.names = FALSE )
 write.table(new_m, file="data_expr.txt", row.names=TRUE, col.names=TRUE)
