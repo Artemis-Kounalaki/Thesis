e7b474740cd8d344316226813aeb37a737535063
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 20:07:22 +0300

    Implementation of CGO_res.py in macaca - mouse

--- CGO_res_m-m.py
+++ CGO_res_m-m.py
@@ -1,5 +1,5 @@
 from CGO_res import CGO_table
 
-#GCOs-nGCOs in RBH mouse-macaca
+# CGO_res.py in macaca - mouse
 
 CGO_table('~/conserved_gene_order1/mus_reference/','CGO_m-m.txt','nCGO_m-m.txt','reciprocal_mus-m.txt','CGO_nCGO_m-m.txt')
