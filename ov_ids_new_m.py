9ddf8ee0e8cfb68693b88c521ada9f53f1e7bb1f
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 20:16:04 +0300

    Implementation of ov_ids_new.py in macaca.

--- ov_ids_new_m.py
+++ ov_ids_new_m.py
@@ -1,6 +1,5 @@
 from ov_ids_new import ov_ids
 
+# Implementation of ov_ids_new.py in macaca.
+
 ov_ids('~/conserved_gene_order1/macaca_reference','Mmul_10','macaca_ids.txt','overlapped_ids_macaca.txt','clean_ids_macaca.txt')
-
-
-
