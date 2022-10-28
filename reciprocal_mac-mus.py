ce02e49fe18e8c29145b19d6f223542f1a222c2e
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 20:59:11 +0300

    Implementation of reciprocal.py in group 3: mouse-macaca

--- reciprocal_mac-mus.py
+++ reciprocal_mac-mus.py
@@ -1,6 +1,5 @@
 from reciprocal import reciprocal
 
+#  Implementation of reciprocal.py in group 3: mouse-macaca
+
 reciprocal('~/conserved_gene_order1/mus_reference', 'results_mus-macaca.txt', 'results_macaca-mus.txt', "reciprocal_mus-mac1.txt","reciprocal_mus-mac2.txt")
-
-
-
