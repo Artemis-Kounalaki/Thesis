71890e3b9e78912e7c258dd69603a436cde0bfca
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 19:38:25 +0300

    Implementation of clear_genome_ovrl.py in macaca.

--- clear_genome_ovrl_m.py
+++ clear_genome_ovrl_m.py
@@ -1,3 +1,5 @@
 from clear_genome_ovrl import clear_genome
 
+#Clear isoforms from macaca proteome 
+
 clear_genome('~/conserved_gene_order1/macaca_reference',"macaca_reference_cleann.fa",'overlapped_ids_macaca.txt','macaca_reference_clean1.fa')
