58c05717e5e625536881e658356726db0a60a665
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 20:16:28 +0300

    Implementation of ov_ids_new.py in mouse.

--- ov_ids_new_mus.py
+++ ov_ids_new_mus.py
@@ -1,4 +1,5 @@
 from ov_ids_new import ov_ids
 
-ov_ids('~/conserved_gene_order1/mus_reference','GRCm38','mus_ids.txt','overlapped_ids_mus.txt','clean_ids_mus.txt')
+# Implementation of ov_ids_new.py in mouse.
 
+ov_ids('~/conserved_gene_order1/mus_reference','GRCm38','mus_ids.txt','overlapped_ids_mus.txt','clean_ids_mus.txt')
