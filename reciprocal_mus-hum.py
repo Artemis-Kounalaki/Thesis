ba98739aeff4bc1422ee451018028846e01c9802
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 20:20:34 +0300

    Implementation of reciprocal.py in group1: human-mouse

--- reciprocal_mus-hum.py
+++ reciprocal_mus-hum.py
@@ -1,4 +1,5 @@
-
 from reciprocal import reciprocal
+
+# Implementation of reciprocal.py in group1: human-mouse
 
 reciprocal('~/conserved_gene_order/mus_reference','results_human-mus.txt', 'results_mus-human.txt' 'reciprocal_hum-mus1.txt', 'reciprocal_hum-mus2.txt')
