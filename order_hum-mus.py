5b51871b69a8244b6ad48aa769dd3e6540493813
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 20:01:51 +0300

    Implementation of order.py in human-mouse.

--- order_hum-mus.py
+++ order_hum-mus.py
@@ -1,5 +1,5 @@
 from order import order
 
-# GCOs-nGCOs in RBH human-mouse
+#GCOs-nGCOs in RBH human-mouse
 
 order('~/conserved_gene_order1/mus_reference','reciprocal_h-mus.txt','~/conserved_gene_order1/human_reference','clean_ids.txt','~/conserved_gene_order1/mus_reference','clean_ids_mus.txt','~/conserved_gene_order1/mus_reference/CGO.txt','~/conserved_gene_order1/mus_reference/nCGO.txt')
