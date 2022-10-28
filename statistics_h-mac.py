84301b728d958039b948de5ff955eeb3e3e34b00
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 20:53:44 +0300

    Implementation of statistics.py in group 1: human-macaca

--- statistics_h-mac.py
+++ statistics_h-mac.py
@@ -1,2 +1,5 @@
 from statistics import stat
+
+# Function with statistics in GCOS/nGCOs in group 1: human-macaca
+
 stat('~/conserved_gene_order1/macaca_reference','statistics_h-mac.txt' ,'CGO_nCGO.txt')
