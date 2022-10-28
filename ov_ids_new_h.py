745c0fdd3e7ec18ab3f1b3a233ad4e86ac5a6454
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 20:15:38 +0300

    Implementation of ov_ids_new.py in human.

--- ov_ids_new_h.py
+++ ov_ids_new_h.py
@@ -1,4 +1,5 @@
 from ov_ids_new import ov_ids
 
+# Implementation of ov_ids_new.py in human.
 
 ov_ids('~/conserved_gene_order1/human_reference','GRCh38','ids.txt','overlapped_ids.txt','clean_ids.txt')
