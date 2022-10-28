b54edd6ec5bac150a0891ba444fce87b6f28e7f3
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 20:35:05 +0300

    Implementation of statistics.py in group 2: human-mouse

--- statistics_h-mus.py
+++ statistics_h-mus.py
@@ -1,2 +1,5 @@
 from statistics import stat
+
+# Function with statistics in GCOS/nGCOs in group 2: human-mouse
+
 stat('~/conserved_gene_order1/mus_reference','statistics_h-mus.txt' ,'CGO_nCGO.txt')
