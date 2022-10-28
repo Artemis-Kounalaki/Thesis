cf16f7286351f8c51a2d1ff7fe2a0959a86d73fc
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 19:59:18 +0300

    Implementation of order.py in human-macaca.

--- order_hum-mac.py
+++ order_hum-mac.py
@@ -1,5 +1,5 @@
 from order import order
 
-#GCOs-nGCOs in RBH human-macaca
+# GCOs-nGCOs in RBH human-macaca
 
 order('~/conserved_gene_order1/macaca_reference','reciprocal_h-m.txt','~/conserved_gene_order1/human_reference','clean_ids.txt','~/conserved_gene_order1/macaca_reference','clean_ids_macaca.txt','~/conserved_gene_order1/macaca_reference/CGO.txt','~/conserved_gene_order1/macaca_reference/nCGO.txt')
