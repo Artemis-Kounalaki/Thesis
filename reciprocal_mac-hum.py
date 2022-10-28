51aa7a2936a824d8a30ec7eaa46be2108f0796de
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 20:20:50 +0300

    Implementation of reciprocal.py in group1: human-macaca

--- reciprocal_mac-hum.py
+++ reciprocal_mac-hum.py
@@ -1,5 +1,5 @@
 from reciprocal import reciprocal
 
-#  Implementation of reciprocal.py in group1: human-mouse
+#  Implementation of reciprocal.py in group1: human-macaca
 
 reciprocal('~/conserved_gene_order/macaca_reference', 'results_human-macaca.txt', 'results_macaca-human.txt', "reciprocal_hum-mac1.txt","reciprocal_hum-mac2.txt")
