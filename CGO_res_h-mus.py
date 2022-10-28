778890ccd4906067cf79e268a0ae68982f7baab4
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 20:06:10 +0300

    Implementation of CGO_res.py in human - mouse

--- CGO_res_h-mus.py
+++ CGO_res_h-mus.py
@@ -1,5 +1,5 @@
 from CGO_res import CGO_table
 
-#GCOs-nGCOs in RBH human-mouse
+# CGO_res.py in human - mouse
 
 CGO_table('~/conserved_gene_order1/mus_reference/','CGO.txt','nCGO.txt','reciprocal_h-mus.txt','CGO_nCGO.txt')
