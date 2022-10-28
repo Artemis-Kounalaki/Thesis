079da8930bc882ad664e9d0c043aa6d0dc94e5a0
Artemis-Kounalaki <kounalaki.artemis@yahoo.com>
2022-10-28 20:02:55 +0300

    Implementation of order.py in macaca-mouse.

--- order_mus-mac.py
+++ order_mus-mac.py
@@ -1,3 +1,5 @@
 from order import order
 
+# GCOs-nGCOs in RBH mouse-macaca
+
 order('~/conserved_gene_order1/mus_reference','reciprocal_mus-m.txt','~/conserved_gene_order1/mus_reference','clean_ids_mus.txt','~/conserved_gene_order1/macaca_reference','clean_ids_macaca.txt','~/conserved_gene_order1/mus_reference/CGO_m-m.txt','~/conserved_gene_order1/mus_reference/nCGO_m-m.txt')
